bd39f2dba7088cdda215ef9f3a59bd0ad0e9fd72
Facundo Arrascaeta <arrascaeta.facu@gmail.com>
2021-10-24 12:41:47 -0300

    Funcionalidad básica de la aplicacación y estilos

--- src/App.js
+++ src/App.js
@@ -1,25 +1,86 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import ReactDOM from 'react-dom';
 
-function App() {
+export const TareaNueva = React.forwardRef(function (props, ref) {
+    const [isHidden, setIsHidden] = React.useState(true);
+    const [value, setValue] = React.useState('');
+    return (
+      <React.Fragment>
+        <input type='text' className={isHidden ? 'hidden' : 'input-new-task'} ref={ref} value={value} onChange={(e) => setValue(e.target.value)} />
+        <button
+          className={isHidden ? 'hidden' : 'btn btn-short-add'}
+          onClick={() => {
+            props.onClick();
+            setValue('');
+            }}>+</button>
+        <button
+          className={isHidden ? 'btn btn-large-add' : 'hidden'}
+          onClick={() => {
+            setIsHidden(!isHidden);
+
+          } }>+ Nueva tarea</button>
+      </React.Fragment>
+    );
+  })
+
+export function ListaTareas() {
+  const [lista, setLista] = React.useState([]);
+  const input = React.useRef();
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <React.Fragment>
+      <div className='lista-tareas'>
+        <ul>
+          {lista.map((item, i) => {
+            return <Tarea 
+                      contenido={item}
+                      key={i}
+                      onClick={() => {
+                        const nuevaLista = [...lista];
+                        const index = nuevaLista.indexOf(item);
+                        nuevaLista.splice(index, 1)
+                        console.log(lista)
+                        setLista(nuevaLista);
+                        console.log(lista)
+                      }}
+                    />
+          })}
+        </ul>
+      </div>
+      <div className='tarea-nueva'>
+        <TareaNueva onClick={() => setLista([...lista, input.current.value])} ref={input}/>
+      </div>
+    </React.Fragment>
+  )
 }
 
-export default App;
+
+export function Tarea(props) {
+  const [isCompleted, setIsCompleted] = React.useState(false);
+  return (
+    <React.Fragment>
+      <div className='wrap-item'>
+        <input type='checkbox' onChange={() => setIsCompleted(!isCompleted)} /><br />
+        <li className={isCompleted ? 'tarea tachado' : 'tarea'}>{props.contenido}</li>
+        <button className={isCompleted ? 'delete' : 'delete hidden' } onClick={() => props.onClick()}><img className='icon' src="http://simpleicon.com/wp-content/uploads/trash.svg"/></button>
+      </div>
+    </React.Fragment>
+  )
+
+}
+
+export function App(props) {
+  return (
+    <React.Fragment>
+      <div className='container'>
+        <div className='cabecera'>
+          <h1 className='titulo'>{props.title}</h1>
+        </div>
+        <ListaTareas />
+      </div>
+    </React.Fragment>
+  )
+}
+
+
+
